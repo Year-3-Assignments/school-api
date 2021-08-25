442808bc36125d21736d634b9f81203881c7720a
parathantl <parathanlive123@gmail.com>
2021-08-26 02:00:16 +0530

    changes

--- config.js
+++ config.js
@@ -5,4 +5,3 @@ export const LOCAL_MONGO_URI = 'mongodb://localhost:27017/schooldb';
 export const PRODUCTION_PORT = 4000;
 export const PRODUCTION_MONGO_URI =
   'mongodb+srv://schooluser:School2021@school-cluster.ggzfd.mongodb.net/schooldb?retryWrites=true&w=majority';
-// export const PRODUCTION_MONGO_URI = 'mongodb://localhost/schooldb';
