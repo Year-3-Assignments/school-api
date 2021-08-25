01104962cccd8368dc57f983a632aab0b79d694b
parathantl <parathanlive123@gmail.com>
2021-08-26 02:04:02 +0530

    Merged full changes

--- config.js
+++ config.js
@@ -4,8 +4,4 @@ export const DEVELOPMENT_MONGO_URI =
 export const LOCAL_MONGO_URI = 'mongodb://localhost:27017/schooldb';
 export const PRODUCTION_PORT = 4000;
 export const PRODUCTION_MONGO_URI =
-<<<<<<< HEAD
   'mongodb+srv://schooluser:School2021@school-cluster.ggzfd.mongodb.net/schooldb?retryWrites=true&w=majority';
-=======
-  'mongodb+srv://schooluser:School2021@school-cluster.ggzfd.mongodb.net/schooldb?retryWrites=true&w=majority';
->>>>>>> develop
